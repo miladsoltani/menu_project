df2390e8d243aa52f69b964da744d37b78914650
milad <milad.soltani77ms@gmail.com>
2022-07-27 09:16:11 -0700

    second change sidebar component

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import pic from '../../images/pic.jpg'
 import styles from './Sidebar.module.css'
 const Sidebar = () => {
@@ -17,12 +17,24 @@ const Sidebar = () => {
                     <span className='fa fa-star' id={styles.fill}></span>
                 </div>
                 <ul>
-                    <li><Link to="/search" className='fa fa-search'> جستجو</Link></li>
-                    <li><Link to="/my-adv" className='fa fa-file-text-o'> آگهی های من</Link></li>
-                    <li><Link to="/agents" className='fa fa-users'> نماینده</Link></li>
-                    <li><Link to="/profile" className='fa fa-user-o'> پروفایل</Link></li>
-                    <li><Link to="/saved" className='fa fa-bookmark-o'> ملک های ذخیره شده</Link></li>
-                    <li><Link to="/recent-search" className='fa fa-book'> جستجوهای اخیر</Link></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/search" className='fa fa-search'> جستجو</NavLink></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/my-adv" className='fa fa-file-text-o'> آگهی های من</NavLink></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/agents" className='fa fa-users'> نماینده</NavLink></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/profile" className='fa fa-user-o'> پروفایل</NavLink></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/saved" className='fa fa-bookmark-o'> ملک های ذخیره شده</NavLink></li>
+                    <li><NavLink style={({isActive})=>{
+                        return {color:isActive ? 'black' : 'white' , backgroundColor:isActive ? 'white' : 'transparent' , borderRadius:isActive? '0px 50px 50px 0':''}
+                    }} to="/recent-search" className='fa fa-book'> جستجوهای اخیر</NavLink></li>
                 </ul>
             </nav>
         </div>
@@ -30,3 +42,9 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+
+
+// padding: 1rem 0.5rem 1rem 7.5rem;
+    // margin: 0rem 2.5rem 0rem 0.5rem;
+    // border-radius: 0px 50px 50px 0;
